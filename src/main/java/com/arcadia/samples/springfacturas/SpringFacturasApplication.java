aed4278f31521efcb5fc8150a3579cd3438cd938
mulatodev <ganaranjo@gmail.com>
2023-03-02 11:28:28 -0500

    Se agregan los fuentes del proyecto spring

---
+++ src/main/java/com/arcadia/samples/springfacturas/SpringFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.arcadia.samples.springfacturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arcadia/samples/springfacturas/SpringFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcadia.samples.springfacturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
